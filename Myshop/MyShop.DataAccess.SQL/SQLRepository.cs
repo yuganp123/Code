fcbe6194afc06ec63bdd7c785d2b3e574a7e3d97
yugz123 <44920206+yugz123@users.noreply.github.com>
2020-04-13 15:45:20 +0200

    SQL DB Added

---
+++ Myshop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,58 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        internal DataContext context;
+        internal DbSet<T> dbset;
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbset = context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return dbset;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbset.Attach(t);
+
+            dbset.Remove(t);
+        }
+
+        public T Find(string Id)
+        {
+            return dbset.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            dbset.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbset.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
+        }
+    }
+}
